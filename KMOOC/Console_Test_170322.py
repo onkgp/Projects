92194e1f86bbd07ac88c994f8fdaa9c34310c9a9
onkgp <mailkgp@gmail.com>
2017-03-28 15:50:33 +0900

    Study_170328

---
+++ KMOOC/Console_Test_170322.py
@@ -0,0 +1,5 @@
+# pylint: disable=C0103
+# pylint: disable=C0301
+print("Enter your name")
+somebody = input() #input 함수는 콘솔창에서 문자열을 입력받는 함수
+print("Hi", somebody)
---
+++ KMOOC/Temperature_170322.py
@@ -0,0 +1,6 @@
+# pylint: disable=C0103
+# pylint: disable=C0301
+print("Hello Wordl", "Hello Again!!!") #콤마(,) 사용할 경우 Print문이 연결됨d
+temperature = float(input("온도를 입력하세요 :")) #input에 사용자가 입력하는 값은 문자열로 인식하므로 반드시 앞에 형변환(여기서는 float)을 해준다
+print(temperature)
+print(type(temperature))
